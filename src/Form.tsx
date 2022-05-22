892f09beb7feed40b4c0b0e955b22f5e235aa1bb
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-22 22:05:07 +0900

    メモ

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,24 +1,27 @@
 import { Stack, TextField, Button } from "@mui/material";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
+//入力の型を決める
 type Inputs = {
   name: string;
 };
 
+//react-hook-form で使う道具を変数に入れる。　検証の定義をする。
 export const Form = () => {
   const {
     control,
     handleSubmit,
     formState: { errors }
-  } = useForm<Inputs>({ defaultValues: { name: "longbridgeyuk" } });
+  } = useForm<Inputs>({ defaultValues: { name: "here!!" } });
 
   const validationRules = {
     name: {
       required: "名前を入力して下さい。",
-      minLength: { value: 4, message: "4文字以上で入力してください。" }
+      minLength: { value: 4, message: "4文字以上で入力してくださいs。" }
     }
   };
 
+  //送信の際の関数
   const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
     console.log(`submit:${data.name}`);
   };
