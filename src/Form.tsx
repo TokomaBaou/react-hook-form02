382b35938932e9009ccbb68a1d582801c6a42ab4
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-22 21:52:47 +0900

    textField

--- src/Form.tsx
+++ src/Form.tsx
@@ -10,12 +10,7 @@ export const Form = () => {
     control,
     handleSubmit,
     formState: { errors }
-  } =
-    useForm <
-    Inputs >
-    {
-      defaultValues: { name: "longbridgeyuk" }
-    };
+  } = useForm<Inputs>({ defaultValues: { name: "longbridgeyuk" } });
 
   const validationRules = {
     name: {
@@ -31,7 +26,31 @@ export const Form = () => {
   return (
     <>
       formです
-      <Stack></Stack>
+      <Stack
+        component="form"
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+        spacing={2}
+        sx={{ m: 2, width: "25ch" }}
+      >
+        <Controller
+          name="name"
+          control={control}
+          rules={validationRules.name}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              type="text"
+              label="名前"
+              error={errors.name !== undefined}
+              helperText={errors.name?.message}
+            />
+          )}
+        />
+      </Stack>
+      <Button variant="contained" type="submit">
+        送信する
+      </Button>
     </>
   );
 };
